a67b6db545e20980676cc48c90670ca32e59a4ec
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-04-29 11:28:46 +0500

    AddHappyPlaceActivity : Added -> Calendar Picker And Populate Our Date Edit Text

--- app/src/main/java/com/example/happy_places_app/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happy_places_app/AddHappyPlaceActivity.kt
@@ -1,21 +1,57 @@
 package com.example.happy_places_app
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.happy_places_app.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     //private var toolBarAddPlace : Toolbar? = null
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
     private var binding : ActivityAddHappyPlaceBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
+        setContentView(binding?.root)
         //toolBarAddPlace = findViewById(R.id.toolbar_add_place)
-        setSupportActionBar(binding!!.toolbarAddPlace)
+        setSupportActionBar(binding?.toolbarAddPlace)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        binding!!.toolbarAddPlace.setNavigationOnClickListener{
+        binding?.toolbarAddPlace?.setNavigationOnClickListener{
             onBackPressed()
         }
+
+
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+        binding?.etDate?.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date ->{
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+    private fun updateDateInView(){
+        val myFormat = "dd.MM.yyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding?.etDate?.setText(sdf.format(cal.time).toString())
     }
 }
