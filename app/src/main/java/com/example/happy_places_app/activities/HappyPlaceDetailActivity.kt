1739251e63a136614515fa434cbf21891b798692
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-05-03 16:03:43 +0500

    Happy Place Detail Activity : Added -> Passing Whole Objects from One Activity To Another - Serializable vs Parcalable

--- app/src/main/java/com/example/happy_places_app/activities/HappyPlaceDetailActivity.kt
+++ app/src/main/java/com/example/happy_places_app/activities/HappyPlaceDetailActivity.kt
@@ -1,12 +1,39 @@
 package com.example.happy_places_app.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.happy_places_app.R
+import com.example.happy_places_app.databinding.ActivityHappyPlaceDetailBinding
+import com.example.happy_places_app.models.HappyPlaceModel
 
 class HappyPlaceDetailActivity : AppCompatActivity() {
+    private var binding : ActivityHappyPlaceDetailBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_happy_place_detail)
+        binding = ActivityHappyPlaceDetailBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        var happyPlaceDetailModel : HappyPlaceModel? = null
+
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)){
+            happyPlaceDetailModel = intent.getParcelableExtra(
+                MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel?
+        }
+        if (happyPlaceDetailModel != null){
+            setSupportActionBar(binding!!.toolbarHappyPlaceDetail)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = happyPlaceDetailModel.title
+
+            binding!!.toolbarHappyPlaceDetail.setNavigationOnClickListener{
+                onBackPressed()
+            }
+
+            binding!!.ivPlaceImage.setImageURI(Uri.parse(happyPlaceDetailModel.image))
+            binding!!.tvDescription.text = happyPlaceDetailModel.description
+            binding!!.tvLocation.text = happyPlaceDetailModel.location
+        }
+
     }
+
 }
